b9c72c6f6fdbeb77c5745b31622bd1aa429eb3d7
rpremisleaner1 <rpremisleaner1@babson.edu>
2022-03-27 12:16:48 -0400

    Create file_1.py

---
+++ Project_files/file_1.py
@@ -0,0 +1,8 @@
+# Imports
+"""
+To begin this project, we must first install the libraries required.
+Since I am using cinemagoer, I ran "pip install cinemagoer"
+"""
+
+from imdb import Cinemagoer
+# numpy import 
