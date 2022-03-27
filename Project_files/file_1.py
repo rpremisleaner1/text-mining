0fb5a6109014a45be5b87148b40f4ffa2bd09a33
rpremisleaner1 <rpremisleaner1@babson.edu>
2022-03-27 19:17:26 -0400

    Update file_1.py

--- Project_files/file_1.py
+++ Project_files/file_1.py
@@ -4,27 +4,34 @@ To begin this project, we must first install the libraries required.
 Since I am using cinemagoer, I ran "pip install cinemagoer"
 """
 
+from asyncio.windows_events import NULL
+from tkinter import NONE
 import imdb
 import string
 import pandas as pd
 import plotnine
+from plotnine import * # visualization like ggplot
 from imdb import Cinemagoer
 
 instance = imdb.Cinemagoer()
 
-movie_1 = instance.search_movie("War Dogs")[0]
-movie_2 = instance.search_movie("21 Jump Street")[0]
-movie_3 = instance.search_movie("Moneyball")[0]
+# movie_1_name = "War Dogs"
+# movie_2_name = "21 Jump Street"
+# movie_3_name = "Moneyball"
 
-print(movie_1.movieID)  # ID is 2005151
-print(movie_2.movieID)  # ID is 1232829
-print(movie_3.movieID)  # ID is 1210166
+# movie_1 = instance.search_movie(movie_1_name)[0]
+# movie_2 = instance.search_movie(movie_2_name)[0]
+# movie_3 = instance.search_movie(movie_3_name)[0]
 
-movie_1_id = movie_1.movieID
-movie_2_id = movie_2.movieID
-movie_3_id = movie_3.movieID
+# print(movie_1.movieID)  # ID is 2005151
+# print(movie_2.movieID)  # ID is 1232829
+# print(movie_3.movieID)  # ID is 1210166
 
-print(movie_1_id)
+# movie_1_id = movie_1.movieID
+# movie_2_id = movie_2.movieID
+# movie_3_id = movie_3.movieID
+
+# print(movie_1_id)
 
 # print(movie_reviews['data']['reviews'][0]['content']) # check 'rating' as well
 # print(movie_reviews['data']['reviews'][1]['content']) # transforming into a list
@@ -46,36 +53,23 @@ def list_reviews(movie_id, num_reviews):
 # print(list_reviews(movie_3_id, 5))
 
 
-listed_review_1 = list_reviews(movie_1_id, 5)
+# listed_review_1 = list_reviews(movie_1_id, 5)
+# listed_review_2 = list_reviews(movie_2_id, 5)
+# listed_review_3 = list_reviews(movie_3_id, 5)
 
-
-def make_list():
-    k = 0
-    new_list = []
-    for line in range(len(listed_review_1)):
-        line = listed_review_1[k]
-        word = line.split()
-        new_list.append(word)
-        # print(listed_review_1[line])
-        k += 1
-    return new_list
-
-
-# print(make_list())
-test_list = make_list()
-
-
-def create_dict():
+def create_dict(listed_review):
     res = {}
-    for line in range(len(listed_review_1)):
-        line = listed_review_1[line]
+    for line in range(len(listed_review)):
+        line = listed_review[line]
         word = line.split()
         for words in word:
             res[words] = res.get(words, 0) + 1
     return res
 
 
-dict_object = create_dict()
+# dict_object_1 = create_dict(listed_review_1)
+# dict_object_2 = create_dict(listed_review_2)
+# dict_object_3 = create_dict(listed_review_3)
 
 
 def create_sorted_dict(a_dict):
@@ -87,7 +81,9 @@ def create_sorted_dict(a_dict):
 
 
 # print(create_sorted_dict(dict_object))
-sorted_dict = create_sorted_dict(dict_object)
+# sorted_dict_1 = create_sorted_dict(dict_object_1)
+# sorted_dict_2 = create_sorted_dict(dict_object_2)
+# sorted_dict_3 = create_sorted_dict(dict_object_3)
 
 
 def process_file(filename):
@@ -131,10 +127,84 @@ def most_common(sorted_dictionary, k_words, excluding_stopwords=False):
     return lst[0:k_words]
 
 
-most_common_list = most_common(sorted_dict, 5, True)
-df = pd.DataFrame(most_common_list, columns = ['frequency', 'word']) # Source: https://www.geeksforgeeks.org/create-a-pandas-dataframe-from-lists/
-print(df)
+# most_common_list_1 = most_common(sorted_dict_1, 5, True)
+# most_common_list_2 = most_common(sorted_dict_2, 5, True)
+# most_common_list_3 = most_common(sorted_dict_3, 5, True)
 
+# df_1 = pd.DataFrame(most_common_list_1, columns = ['frequency', 'word']) # Source: https://www.geeksforgeeks.org/create-a-pandas-dataframe-from-lists/
+# df_1['movie_name'] = movie_1_name
+
+# df_2 = pd.DataFrame(most_common_list_2, columns = ['frequency', 'word'])
+# df_2['movie_name'] = movie_2_name
+
+# df_3 = pd.DataFrame(most_common_list_3, columns = ['frequency', 'word'])
+# df_3['movie_name'] = movie_3_name
+
+# intermediate_df = df_1.append(df_2, ignore_index = True)
+# full_df = intermediate_df.append(df_3, ignore_index = True)
+
+
+# print(df_1)
+# print(df_2)
+# print(df_3)
+# print(full_df)
+
+
+# print((ggplot(full_df, aes(y = 'frequency', x = 'word', fill = 'movie_name', color = 'movie_name')) # https://plotnine.readthedocs.io/en/stable/
+# + geom_bar(stat = 'identity') 
+# + theme(legend_position = 'bottom', legend_title = element_blank())
+# + scale_y_continuous(minor_breaks = NULL)
+# + labs(x = '', y = '', title = 'Frequency of Word (Y) vs. Word Name (X) by Movie Name (Colors) and Movie Name (Facets)')
+# + facet_wrap('~movie_name', ncol = 3, scales = 'free_x')))
+
+
+def main():
+    movie_1_name = "War Dogs"
+    movie_2_name = "21 Jump Street"
+    movie_3_name = "Moneyball"
+
+    movie_1 = instance.search_movie(movie_1_name)[0]
+    movie_2 = instance.search_movie(movie_2_name)[0]
+    movie_3 = instance.search_movie(movie_3_name)[0]
+
+    movie_1_id = movie_1.movieID
+    movie_2_id = movie_2.movieID
+    movie_3_id = movie_3.movieID
+
+    listed_review_1 = list_reviews(movie_1_id, 5)
+    listed_review_2 = list_reviews(movie_2_id, 5)
+    listed_review_3 = list_reviews(movie_3_id, 5)
+
+    dict_object_1 = create_dict(listed_review_1)
+    dict_object_2 = create_dict(listed_review_2)
+    dict_object_3 = create_dict(listed_review_3)
+
+    sorted_dict_1 = create_sorted_dict(dict_object_1)
+    sorted_dict_2 = create_sorted_dict(dict_object_2)
+    sorted_dict_3 = create_sorted_dict(dict_object_3)
+
+    most_common_list_1 = most_common(sorted_dict_1, 5, True)
+    most_common_list_2 = most_common(sorted_dict_2, 5, True)
+    most_common_list_3 = most_common(sorted_dict_3, 5, True)
+
+    df_1 = pd.DataFrame(most_common_list_1, columns = ['frequency', 'word']) # Source: https://www.geeksforgeeks.org/create-a-pandas-dataframe-from-lists/
+    df_1['movie_name'] = movie_1_name
+
+    df_2 = pd.DataFrame(most_common_list_2, columns = ['frequency', 'word'])
+    df_2['movie_name'] = movie_2_name
+
+    df_3 = pd.DataFrame(most_common_list_3, columns = ['frequency', 'word'])
+    df_3['movie_name'] = movie_3_name
+
+    intermediate_df = df_1.append(df_2, ignore_index = True)
+    full_df = intermediate_df.append(df_3, ignore_index = True)
+
+    print((ggplot(full_df, aes(y = 'frequency', x = 'word', fill = 'movie_name', color = 'movie_name')) # https://plotnine.readthedocs.io/en/stable/
+    + geom_bar(stat = 'identity') 
+    + theme(legend_position = 'bottom', legend_title = element_blank())
+    + scale_y_continuous(minor_breaks = NULL)
+    + labs(x = '', y = '', title = 'Frequency of Word (Y) vs. Word Name (X) by Movie Name (Colors) and Movie Name (Facets)')
+    + facet_wrap('~movie_name', ncol = 3, scales = 'free_x')))
 
 # if __name__ == "__main__":
 #     main()
